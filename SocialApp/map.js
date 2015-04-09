631cfa43f9a1fbb680fe7a4658ad32d2108fe019
Mikael Nelsson <mikael.nelsson@live.se>
2015-04-09 12:05:51 +0200

    en karta

--- SocialApp/map.js
+++ SocialApp/map.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-   alert('gustav!!!!')
+   function initialize(){
+       var mapProp= {
+           center:new google.maps.LatLng(59.858060, 17.639740),
+           zoom:13,
+           mapTypeId: google.maps.MapTypeId.ROADMAP
+       };
+       var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+   }
+    initialize();
 });
