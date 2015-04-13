4c3c6705c4397ef10965afe3878286d6982e2ebf
Gustafv <j.gustav.karlsson@gmail.com>
2015-04-13 09:36:48 +0200

    nån ändring?

--- SocialApp/twitter-requests.js
+++ SocialApp/twitter-requests.js
@@ -1,3 +1,6 @@
-/**
- * Created by gustavkarlsson on 15-04-09.
- */
+var myRequest = new XMLHttpRequest();
+myRequest.open("POST","http://api.twitter.com/oauth2/token?grant_type=client_credentials", true);
+myRequest.setRequestHeader("Authentication", "TjE2ZXk4UHBEdHFYSHJQbEJTeloyWUpEbzp4TEdzN3J3Q0pYNFk5Yzg4WFkwTnNwZmVHazJ3MnlITDJhN2ZxcjdUUXk1NDNXblRQVg==");
+myRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8" );
+myRequest.send();
+alert(myRequest.response);
